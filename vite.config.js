b4101ad844860cb8622b033a2e259f06b2d899eb
MattLin <85590065+marttrach@users.noreply.github.com>
2025-04-10 18:56:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
+// 如果你是部署到自訂網域（如 book.junchan.blog），請用 base: "/"
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  base: "/", // <--- 加上這一行是關鍵
+  plugins: [react()],
 });
